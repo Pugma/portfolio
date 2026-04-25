48399a585890be39f3218c0f16bb9bd4ce625a19
Pugma <132571355+Pugma@users.noreply.github.com>
2026-04-26 06:33:13 +0900

    feat(works): add works entries

--- src/data/works.ts
+++ src/data/works.ts
@@ -1,3 +1,7 @@
+import Pugma from "../assets/Pugma.png";
+import traQ from "../assets/traQ.png";
+import traQgazerIcon from "../assets/traQgazer_icon.svg";
+
 type Work = {
   title: string;
   description: string;
@@ -15,4 +19,69 @@ export type WorkTags = {
   techStack: string[];
 };
 
-export const works: Work[] = [];
+export const works: Work[] = [
+  {
+    title: "traPortfolio",
+    description:
+      "traP部員が部内での活動や、その制作物を外部にアピールできるプラットフォーム",
+    thumbnail: undefined,
+    image: [],
+    tags: {
+      duration: "long-term",
+      teamSize: "team",
+      techStack: ["Vue", "TypeScript", "Go"],
+    },
+  },
+  {
+    title: "traQ gazer",
+    description:
+      "直近の投稿のうち、設定した文字列を含むものを通知する traQ bot",
+    thumbnail: traQgazerIcon,
+    image: [],
+    tags: {
+      duration: "hackathon",
+      teamSize: "team",
+      techStack: ["Tailwind CSS", "headless UI"],
+    },
+  },
+  {
+    title: "traPortal v2",
+    description:
+      "部員管理システム。新たに OIDC の IdP としても機能するように開発中",
+    thumbnail: undefined,
+    image: [],
+    tags: {
+      duration: "long-term",
+      teamSize: "team",
+      techStack: ["Vue", "TypeScript", "Go", "OAuth 2.1", "OIDC 1.0"],
+    },
+  },
+  {
+    title: "ぷぐま Portfolio",
+    description: "このサイト",
+    thumbnail: Pugma,
+    image: [],
+    tags: {
+      duration: "long-term",
+      teamSize: "solo",
+      techStack: [
+        "Astro",
+        "TypeScript",
+        "Tailwind CSS",
+        "mise",
+        "Cloudflare Workers (Pages)",
+      ],
+    },
+  },
+  {
+    title: "traQ",
+    description: "サークル内製チャットアプリ",
+    thumbnail: traQ,
+    image: [],
+    tags: {
+      duration: "long-term",
+      teamSize: "team",
+      techStack: ["Vue", "TypeScript", "Go", "echo"],
+    },
+  },
+];
