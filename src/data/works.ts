16d6b3732688705bd34a363d42436be8fddd6a5b
Pugma <132571355+Pugma@users.noreply.github.com>
2026-04-26 22:05:13 +0900

    feat(works): add dialog for tile view

--- src/data/works.ts
+++ src/data/works.ts
@@ -11,6 +11,25 @@ type Work = {
     src: ImageMetadata;
   }>;
   tags: WorkTags;
+  roles: WorkRoles;
+  links: WorkLinks;
+};
+
+export type WorkRoles = {
+  current: string[];
+  past: string[];
+};
+
+type LinkItem = {
+  subLabel: string | undefined;
+  url: string;
+};
+
+export type WorkLinks = {
+  product: LinkItem[];
+  blog: LinkItem[];
+  source: LinkItem[];
+  others: LinkItem[];
 };
 
 export type WorkTags = {
@@ -19,8 +38,8 @@ export type WorkTags = {
   techStack: string[];
 };
 
-export const works: Work[] = [
-  {
+export const works = {
+  traportfolio: {
     title: "traPortfolio",
     description:
       "traP部員が部内での活動や、その制作物を外部にアピールできるプラットフォーム",
@@ -31,8 +50,18 @@ export const works: Work[] = [
       teamSize: "team",
       techStack: ["Vue", "TypeScript", "Go"],
     },
+    roles: {
+      current: [],
+      past: [],
+    },
+    links: {
+      product: [],
+      source: [],
+      blog: [],
+      others: [],
+    },
   },
-  {
+  "traq-gazer": {
     title: "traQ gazer",
     description:
       "直近の投稿のうち、設定した文字列を含むものを通知する traQ bot",
@@ -43,8 +72,18 @@ export const works: Work[] = [
       teamSize: "team",
       techStack: ["Tailwind CSS", "headless UI"],
     },
+    roles: {
+      current: [],
+      past: [],
+    },
+    links: {
+      product: [],
+      source: [],
+      blog: [],
+      others: [],
+    },
   },
-  {
+  "traportal-v2": {
     title: "traPortal v2",
     description:
       "部員管理システム。新たに OIDC の IdP としても機能するように開発中",
@@ -55,8 +94,18 @@ export const works: Work[] = [
       teamSize: "team",
       techStack: ["Vue", "TypeScript", "Go", "OAuth 2.1", "OIDC 1.0"],
     },
+    roles: {
+      current: [],
+      past: [],
+    },
+    links: {
+      product: [],
+      source: [],
+      blog: [],
+      others: [],
+    },
   },
-  {
+  "pugma-portfolio": {
     title: "ぷぐま Portfolio",
     description: "このサイト",
     thumbnail: Pugma,
@@ -72,8 +121,15 @@ export const works: Work[] = [
         "Cloudflare Workers (Pages)",
       ],
     },
+    roles: { current: [], past: [] },
+    links: {
+      product: [],
+      source: [],
+      blog: [],
+      others: [],
+    },
   },
-  {
+  traq: {
     title: "traQ",
     description: "サークル内製チャットアプリ",
     thumbnail: traQ,
@@ -83,5 +139,14 @@ export const works: Work[] = [
       teamSize: "team",
       techStack: ["Vue", "TypeScript", "Go", "echo"],
     },
+    roles: { current: [], past: [] },
+    links: {
+      product: [],
+      source: [],
+      blog: [],
+      others: [],
+    },
   },
-];
+} satisfies Record<string, Work>;
+
+export type WorkKey = keyof typeof works;
